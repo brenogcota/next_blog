ee46d4b85e08952f1c3a7954d63321c3cfc24823
Breno Cota <brenogcota@gmail.com>
2022-07-05 18:03:33 -0300

    feat: create blog page

---
+++ components/BlogList/index.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Text from "ui/Text";
+import { Post, Posts, Tag } from "./stitches";
+
+const textCustomStyle = {
+    '&:hover': {
+        color: '$primary'
+    }
+}
+
+const BlogList = () => {
+  
+  return (
+    <Posts>
+        <Post>
+            <Link href="/blog/the-css-grid-minmax-function-explained">
+                <Text 
+                    size="lg"
+                    css={textCustomStyle}
+                >
+                    The CSS grid minmax() function explained
+                </Text>
+            </Link>
+            <Tag>CSS</Tag> <Tag>React</Tag> <Tag>Javascript</Tag>
+        </Post>
+    </Posts>
+  );
+}
+
+export default BlogList;

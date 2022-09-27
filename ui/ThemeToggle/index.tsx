ab38323988c61637bf08e77ae2388979f230ef5a
Marcelo Oliveira <marcelotk_15@hotmail.com>
2022-09-27 18:52:39 -0300

    🐛 FIX: theme toogle without value

--- ui/ThemeToggle/index.tsx
+++ ui/ThemeToggle/index.tsx
@@ -1,18 +1,25 @@
+import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
-import { Switch, Thumb } from './stitches'
+
+import { Switch, Thumb } from './stitches';
 
 const ThemeToggle = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  const toggleTheme = () =>
-    setTheme(theme === 'light' ? 'dark' : 'light');
+
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  if (!mounted) return null;
 
   return (
     <Switch 
-      onCheckedChange={() => toggleTheme() }
+      onCheckedChange={() => setTheme(theme === 'light' ? 'dark' : 'light') }
       defaultChecked={theme === 'dark'}
     >
-        <Thumb />
+      <Thumb />
     </Switch>
   );
 }
